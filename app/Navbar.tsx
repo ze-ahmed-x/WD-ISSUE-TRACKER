40eb683f17e3b7bd061f02d4f127cbb4d20a8d5a
Zeshan Ahmed <ze.ahmed.x@protonmail.com>
2024-01-03 14:13:48 +0500

    style active link in navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,13 @@
+'use client'
+import classNames from 'classnames';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { IoBug } from "react-icons/io5";
 
+
 const Navbar = () => {
+    const currentPath = usePathname();
     const links = [
         { name: 'Dashboard', href: '/' },
         { name: 'Issues', href: '/issues' },
@@ -12,11 +17,22 @@ const Navbar = () => {
         <Link href='/'><IoBug /></Link>
         <ul className='flex space-x-6 '>
             {links.map(link => (
-                <li className='text-zinc-500 hover:text-zinc-800 transition-colors' key={link.name}>
+                <li className={
+                    // `${link.href === currentPath? 'text-zinc-800' : 'text-zinc-500' } hover:text-zinc-800 transition-colors`
+                    classNames(
+                        {
+                            'text-zinc-900': link.href === currentPath,
+                            'text-zinc-500': link.href!== currentPath,
+                            'hover:text-zinc-800 transition-colors': true
+                        }
+
+                    )
+                } 
+                key={link.name}>
                     <Link href={link.href}>{link.name}</Link>
                 </li>
             ))}
-            {/* <li><Link href='/'>Dashboard</Link></li>
+            {/* <li><Link href=`/'>Dashboard</Link></li>
             <li><Link href='/issues'>Issues</Link></li> */}
         </ul>
     </nav>
