717cc2da7c8c52b1183d593b184a7370c7f60787
Zeshan Ahmed <ze.ahmed.x@protonmail.com>
2024-01-03 20:42:38 +0500

    Build the new issue page

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation';
 import React from 'react'
 import { IoBug } from "react-icons/io5";
+import { Separator } from '@radix-ui/themes';
 
 
 const Navbar = () => {
@@ -17,6 +18,7 @@ const Navbar = () => {
         <Link href='/'><IoBug /></Link>
         <ul className='flex space-x-6 '>
             {links.map(link => (
+                <>
                 <li className={
                     // `${link.href === currentPath? 'text-zinc-800' : 'text-zinc-500' } hover:text-zinc-800 transition-colors`
                     classNames(
@@ -31,6 +33,8 @@ const Navbar = () => {
                 key={link.name}>
                     <Link href={link.href}>{link.name}</Link>
                 </li>
+                <Separator className='self-center' orientation="vertical" />
+                </>
             ))}
             {/* <li><Link href=`/'>Dashboard</Link></li>
             <li><Link href='/issues'>Issues</Link></li> */}
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 
 const Issues = () => {
   return (
-    <div className='m-10'>
-      <Button>new issue</Button>
+    <div>
+      <Button><Link href='/issues/new' > New Issue </Link> </Button>
     </div>
   )
 }
