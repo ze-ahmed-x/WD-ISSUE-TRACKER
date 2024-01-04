43aeb99b87831d75d6273a879b276770b0ce1556
Zeshan Ahmed <ze.ahmed.x@protonmail.com>
2024-01-04 21:32:39 +0500

    extract the error message component

---
+++ app/components/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+import React, { PropsWithChildren } from 'react'
+import { Text } from '@radix-ui/themes'
+
+const ErrorMessage = ({children} : PropsWithChildren) => {
+if (!children) {
+    return;
+}
+  return (
+    <Text as='p' color='red'> {children} </Text>
+  )
+}
+
+export default ErrorMessage
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -8,6 +8,7 @@ import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/ValidationSchemas';
 import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 // interface IssueForm {
@@ -56,13 +57,15 @@ const CreateNewIssue = () => {
                     <TextField.Input placeholder='Title' {...register("title")} />
                 </TextField.Root>
 
-                {errors.title && <Text as='p' color='red'>{errors.title.message}</Text>}
+                {/* {errors.title && <Text as='p' color='red'>{errors.title.message}</Text>} */}
+                <ErrorMessage>{errors.title?.message} </ErrorMessage>
 
                 <Controller name='description' control={control} render={({ field }) => (
                     <SimpleMDE placeholder="Description..." {...field} />
                 )} />
 
-                {errors.description && <Text as='p' color='red'>{errors.description.message}</Text>}
+                {/* {errors.description && <Text as='p' color='red'>{errors.description.message}</Text>} */}
+                <ErrorMessage>{errors.description?.message} </ErrorMessage>
                 
                 <Button>Submit New Issue</Button>
             </form>
